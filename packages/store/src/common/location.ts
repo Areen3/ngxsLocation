9320d24fea5a9150cbcb84e35fd54fef4cad88ee
Piotr Such <piotr@apr.pl>
2023-07-24 21:10:39 +0200

    feat: add array string param to ngxs location

--- packages/store/src/common/location.ts
+++ packages/store/src/common/location.ts
@@ -5,26 +5,34 @@
 
 export class SingleLocation {
   private _path: string;
+
   constructor(path: string) {
     this._path = this.validate(path);
   }
+
   get path(): string {
     return this._path;
   }
 
-  static getLocation(path: string, childName?: string): SingleLocation {
-    const current = childName === undefined ? path : path + '.' + childName;
+  static getLocation(fragments: Array<string>): SingleLocation;
+  static getLocation(path: string, childName?: string): SingleLocation;
+  static getLocation(path: string | Array<string>, childName?: string): SingleLocation {
+    const myPath: string = Array.isArray(path) ? path.join('.') : path;
+    const current = childName === undefined ? myPath : myPath + '.' + childName;
     return new SingleLocation(current);
   }
+
   getParentName(): string {
     const tab: string[] = this._path.split('.');
     return tab.pop()!;
   }
+
   getParentPath(): SingleLocation {
     const tab: string[] = this._path.split('.');
     tab.pop();
     return SingleLocation.getLocation(tab.join('.'));
   }
+
   getChildLocation(childName: string): SingleLocation {
     return SingleLocation.getLocation(`${this._path}.${childName}`);
   }
@@ -32,6 +40,7 @@ export class SingleLocation {
   getNeighborLocation(childName: string): SingleLocation {
     return SingleLocation.getLocation(this._path).getParentPath().getChildLocation(childName);
   }
+
   getLocation(): SingleLocation {
     return SingleLocation.getLocation(this._path);
   }
