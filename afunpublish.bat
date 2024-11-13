594ceac6e220c12cdff9544abc26997561e0922f
Piotr Such <34617740+Areen3@users.noreply.github.com>
2024-11-13 11:32:20 +0100

    chore: make unpublish bat

---
+++ afunpublish.bat
@@ -0,0 +1,26 @@
+cd @ngxs
+cd store
+call npm unpublish --force
+cd ..
+cd devtools-plugin
+call npm unpublish --force
+cd ..
+cd form-plugin
+call npm unpublish --force
+cd ..
+cd hmr-plugin
+call npm unpublish --force
+cd ..
+cd logger-plugin
+call npm unpublish --force
+cd ..
+cd router-plugin
+call npm unpublish --force
+cd ..
+cd storage-plugin
+call npm unpublish --force
+cd ..
+cd websocket-plugin
+call npm unpublish --force
+cd ..
+cd ..
